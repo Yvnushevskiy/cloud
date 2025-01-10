c15f53352855f4140f29d3911e555beee1214996
SystemX <k.yanushevskiy@gmail.com>
2025-01-10 22:13:35 +0400

    lets start

---
+++ src/main/java/com/example/demo/config/RedisConfig.java
@@ -0,0 +1,57 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.data.redis.config.annotation.web.http.RedisHttpSessionConfiguration;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableRedisHttpSession
+@EnableCaching
+public class RedisConfig {
+    @Value("${SESSION_MAX_INACTIVE_INTERVAL_IN_SECONDS}")
+    private String SESSION_MAX_INACTIVE_INTERVAL_IN_SECONDS;
+
+
+
+    @Bean
+    public RedisConnectionFactory connectionFactory() {
+        return new LettuceConnectionFactory("localhost", 6379);
+    }
+    @Bean
+    public RedisHttpSessionConfiguration redisHttpSessionConfiguration() {
+        RedisHttpSessionConfiguration config = new RedisHttpSessionConfiguration();
+        config.setMaxInactiveInterval(Duration.ofSeconds(Long.parseLong(SESSION_MAX_INACTIVE_INTERVAL_IN_SECONDS)));
+        return config;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration defaults = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(Long.parseLong(SESSION_MAX_INACTIVE_INTERVAL_IN_SECONDS)))
+                .enableTimeToIdle();
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaults)
+                .build();
+    }
+
+
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+}
